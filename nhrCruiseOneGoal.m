32464c0e2cd15934a5852e64b4e0b8f2b3196590
mraafat89 <mahmoudraafat89@gmail.com>
2017-10-17 09:54:33 -0400

    reaction times 300 ms

--- nhrCruiseOneGoal.m
+++ nhrCruiseOneGoal.m
@@ -1,9 +1,9 @@
 function [robot,vx,vy]= nhrCruiseOneGoal(goal, robot, error, integral, derivative)
     %Init Control Parameters
 %    kThetaP = 2.5;
-    kVP = 0.7;
-    kVI = 0.6;
-    kVD = 0.5;
+    kVP = 150;
+    kVI = 0;
+    kVD = 0.05;
     %calculate new robot angle
     thetaNew = atan2(goal.y - robot.y , goal.x - robot.x);
     % calculate new robot velocity
@@ -11,5 +11,11 @@ function [robot,vx,vy]= nhrCruiseOneGoal(goal, robot, error, integral, derivativ
     %update robot 
     vx = v * cos(thetaNew);
     vy = v * sin(thetaNew);
+    if(vx > 10)
+        vx = 10;
+    elseif (vx < -10)
+        vx = -10;
+    end
 end
+
 
